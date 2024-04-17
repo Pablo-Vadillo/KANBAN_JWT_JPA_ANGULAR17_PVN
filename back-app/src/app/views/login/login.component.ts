a48df84a6e22047f372605ff09f6aa6326123bed
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-04-17 11:08:50 +0100

    feat: basic login and basic redirect done

--- back-app/src/app/views/login/login.component.ts
+++ back-app/src/app/views/login/login.component.ts
@@ -1,12 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  //providers:[ApiService],
+  imports: [FormsModule,HttpClientModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm ={
+    username: "",
+    password: ""
+  };
+
+  constructor(
+    private router: Router,
+  ) {}
+
+
+login() {
+  this.router.navigate(['/mainScreen'])
+}
+
+register() {
+  this.router.navigate(['/register']);
+}
+
+
+redirectTo() {
+  this.router.navigate(['/mainScreen'])
+  }
 
 }
