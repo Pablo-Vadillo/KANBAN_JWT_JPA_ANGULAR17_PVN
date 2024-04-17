0e3e164ab8e4867917347de81ae7d9b41d1193e7
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-04-17 11:29:06 +0100

    feat: basic register and basic redirect done

--- back-app/src/app/views/register/register.component.ts
+++ back-app/src/app/views/register/register.component.ts
@@ -1,12 +1,40 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  //providers:[ApiService],
+  imports: [FormsModule,HttpClientModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm ={
+    username: "",
+    password: "",
+    email:""
+  };
+
+  constructor(
+    private router: Router,
+  ) {}
+
+
+login() {
+  this.router.navigate(['/login'])
+}
+
+register() {
+  this.router.navigate(['/mainScreen']);
+}
+
+redirectTo() {
+  this.router.navigate(['/mainScreen'])
+  }
+
 
 }
