0c2e5431700cc1489d0a32450c4d338bbc0aa9f2
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-04-17 09:39:41 +0100

    feat: basic routing done

--- back-app/src/app/app.routes.ts
+++ back-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainScreenComponent } from './views/main-screen/main-screen.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { UserInfoComponent } from './views/user-info/user-info.component';
+import { KanbanScreenComponent } from './views/kanban-screen/kanban-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    { path: "register", component: RegisterComponent},
+    { path: "login", component: LoginComponent},
+    { path: "mainScreen", component: MainScreenComponent},//anActivate: [AuthGuard]}
+    { path: "userInfo", component: UserInfoComponent},//anActivate: [AuthGuard]}
+    { path: "kanbanScreen", component: KanbanScreenComponent}//anActivate: [AuthGuard]}
+];
